643df3275b1e85e1ee33a1a1796c4aab6082a2df
Yuci Gou <ygou@ebi.ac.uk>
2018-10-16 12:47:39 +0100

    Test any given URL instead of hard-coding

--- cypress/integration/article_page_spec.js
+++ cypress/integration/article_page_spec.js
@@ -1,11 +1,13 @@
+const BASE_URL = Cypress.env('BASE_URL')
+
 describe('Article Page', function() {
   it('Ensures abstract is loaded', function() {
-    cy.visit('https://beta.europepmc.org/article/MED/29161421')
+    cy.visit(BASE_URL + '/article/MED/29161421')
     cy.get('div.abstract').should('contain', 'Text-mined biological concepts, including database accession numbers')
   })
 
   it('Ensures full text is loaded', function() {
-    cy.visit('https://beta.europepmc.org/article/MED/29161421')
+    cy.visit(BASE_URL + '/article/MED/29161421')
     cy.get('a[href="#free-full-text"]').click()
     cy.get('div#fulltextcontent').should('contain', 'The content scope of Europe PMC covers both abstracts and full text articles.')
   })
--- cypress/integration/search_page_spec.js
+++ cypress/integration/search_page_spec.js
@@ -1,6 +1,8 @@
+const BASE_URL = Cypress.env('BASE_URL')
+
 describe('Search Page', function() {
   it('Ensures search works', function() {
-    cy.visit('https://beta.europepmc.org/')
+    cy.visit(BASE_URL)
     cy.get('input[name="searchString"]').type('Yuci Gou')
     cy.get('button[type="submit"]').click()
 	  cy.get('div.author-affiliation').should('contain', 'European Bioinformatics Institute')
